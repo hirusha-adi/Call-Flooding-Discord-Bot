1733a1277a5bd73f6b2bd617df545d0a7648fc64
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-07 15:53:11 +0530

    test

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import json
 import os
 import subprocess
+import platform
+from threading import Thread
 
 import discord
 import flask
@@ -20,6 +22,30 @@ with open(token_filename, "r", encoding="utf-8") as _token_file:
 
 
 client = commands.Bot(command_prefix=config['prefix'])
+app = flask.Flask(__name__)
 
 
+def run_web_app():
+    app.run(host='0.0.0.0', port=8090)
+
+
+def run_web_app_threaded():
+    t = Thread(target=run_web_app)
+    t.start()
+
+
+@app.route("/")
+def web_index():
+    return "Hello World"
+
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user.name}')
+    print(f'Discord.py API version: {discord.__version__}')
+    print(f'Python version: {platform.python_version()}')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"teamsds.net/discord"))
+    print('Bot is ready!')
+
+run_web_app_threaded()
 client.run(TOKEN)
